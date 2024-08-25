284730cf5a1808dd3d29ae019976d1eff0d9dc09
zuedev <zuedev@gmail.com>
2024-08-25 01:43:56 +0100

    only allow one video#

--- source/src/components/VideoBackground/index.js
+++ source/src/components/VideoBackground/index.js
@@ -1,10 +1,5 @@
 export default function VideoBackground(props) {
-  let srcArray = props?.srcArray || [
-    "/4198845-hd_1920_1080_25fps.mp4",
-    "/15283169-hd_1080_1920_30fps.mp4",
-    "/11066399-hd_1920_1080_60fps.mp4",
-    "/10222930-hd_1920_1080_30fps.mp4",
-  ];
+  let srcArray = props?.srcArray || ["/4198845-hd_1920_1080_25fps.mp4"];
   let src = props?.src || srcArray[Math.floor(Math.random() * srcArray.length)];
   let type = props?.type || "video/mp4";
 
